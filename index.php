b7d6fbb3ffdb879b0726a4230017feefbd030e1d
farantDEV <34941533+farantDEV@users.noreply.github.com>
2021-02-16 17:19:56 +0100

    first commit
    Page setup and buttons on nav bar

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Organisation et décoration de la maison"/>
+        
+        <link rel="icon" type="image/svg" href="#">
+        <link href="style.css" rel="stylesheet">
+        
+        <title>La vie d'intérieur</title>
+
+    </head>
+    <body>
+        <header>
+            <nav class="navbar">
+                <div class="container ronds">
+                    <button class="email-icon btn" type="button">
+                        <img src="assets/icon/mail.svg" alt="email icon" class="icons">
+                    </button>
+                    <button class="phone-icon btn" type="button">
+                        <img src="assets/icon/phone.svg" alt="phone icon" class="icons">
+                    </button>
+                    <button class="media-icon btn" type="button">
+                        <img src="assets/icon/media.svg" alt="media icon" class="icons">
+                    </button>
+                    
+                </div>
+            </nav>
+        </header>
+    </body>
+    
+</html>
