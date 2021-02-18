db62ac80372602e6fb9203f6f3a8e970b2d5b041
farantDEV <34941533+farantDEV@users.noreply.github.com>
2021-02-18 19:22:49 +0100

    animation commit
    Animation des textes et des boutons

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         
         <link rel="icon" type="image/svg" href="assets/favicon/toits-lvi.svg">
         <link href="style.css" rel="stylesheet">
+        <link rel="stylesheet"    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
         
         <title>La vie d'intérieur</title>
 
@@ -15,13 +16,13 @@
         <header>
             <nav class="navbar">
                 <div class="container ronds">
-                    <button class="email-icon btn" type="button">
+                    <button class="email-icon btn animate__animated animate__fadeIn" type="button">
                         <img src="assets/icon/mail.svg" alt="email icon" class="icons">
                     </button>
-                    <button class="phone-icon btn" type="button">
+                    <button class="phone-icon btn animate__animated animate__fadeIn" type="button">
                         <img src="assets/icon/phone.svg" alt="phone icon" class="icons">
                     </button>
-                    <button class="media-icon btn" type="button">
+                    <button class="media-icon btn animate__animated animate__fadeIn" type="button">
                         <img src="assets/icon/media.svg" alt="media icon" class="icons">
                     </button>
                 </div>
@@ -30,19 +31,22 @@
         </header>
         <main class="hero">
             <div class="hero-text">
-                <h1><span class="bcs">Bien chez soi ,</span> <span class="bes">bien en soi !</span></h1>
-                <h2>Le corps est la demeure 
+                <h1 class="animate__animated animate__fadeInLeft"><span class="bcs ">Bien chez soi ,</span> <span class="bes animate__animated animate__fadeIn">bien en soi !</span></h1>
+                <h2 class="bcs animate__animated animate__backInUp">Le corps est la demeure 
                     de l’âme Comme la maison 
                     est celle du corps.</h2>
             </div>
             <div class="promo">
-                <h3>Nouveaux services en ligne disponibles prochainement</h3>
+                <h3 class="animate__animated animate__flipInX">Nouveaux services en ligne disponibles prochainement</h3>
                 <img class="circles" src="assets/logo/circles.gif" alt="animated circles logo LVI"/>
-                <p>Renseignez votre email pour recevoir des promotions exclusives</p>
-                <form method="post">
-                    <input class="textarea" value="Entrez votre email" type="email" id="email" onfocus="if(this.value == 'Entrez votre email')this.value = '';" />
-                    <input class="form-btn" type="button" value="Je veux !" onclick="sendEmail()"/>
-                </form>
+                <div class="contact-form animate__animated animate__backInUp">
+                    <p>Renseignez votre email pour recevoir des promotions exclusives</p>
+                    <form method="post">
+                        <input class="textarea" value="Entrez votre email" type="email" id="email" onfocus="if(this.value == 'Entrez votre email')this.value = '';" />
+                        <input class="form-btn" type="button" value="Je veux !" onclick="sendEmail()"/>
+                    </form>
+                </div>
+                
             </div>
         </main>
         <aside class="contact">
