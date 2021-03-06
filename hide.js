f662a53c30106ae51ad6ce3817b96dc9b3eb7055
farantDEV <34941533+farantDEV@users.noreply.github.com>
2021-03-06 15:40:18 +0100

    cgu+scroll
    changement page php->html
    ajout page cgu+css
    effet scroll sur barnav et logo

--- hide.js
+++ hide.js
@@ -1,8 +1,37 @@
-document.getElementById('button')
-.addEventListener('onclick', function(event) {
-   event.preventDefault();
-   const backHome = document.getElementById('backHome');
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.addEventListener('resize', resizeOnScroll);
 
-      backHome.style.visibility = 'visible';
+function resizeOnScroll() {
+  if (window.matchMedia("(max-width: 575.98px)").matches){
+    window.onscroll = function() {scrollFunction()};
 
-});
+    function scrollFunction() {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("logobar").style.height = "11.2em";
+          document.getElementById("lvi").style.height = "195px";
+          document.getElementById('rd-container').style.margin ="2%";
+      } else {
+        document.getElementById("logobar").style.height = "12.4em";
+        document.getElementById("lvi").style.height = "220px";
+        document.getElementById('rd-container').style.margin ="3%";
+      }
+    }
+
+  }else {
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("logobar").style.height = "6em";
+          document.getElementById("lvi").style.height = "160px";
+          document.getElementById('rd-container').style.margin ="2%";
+      } else {
+        document.getElementById("logobar").style.height = "9em";
+        document.getElementById("lvi").style.height = "240px";
+        document.getElementById('rd-container').style.margin ="3%";
+      }
+    }
+
+  }
+
+}
