f027cf581eb3dc92c2dfad4048313b102aa38191
farantDEV <34941533+farantDEV@users.noreply.github.com>
2021-03-08 16:25:32 +0100

    opening
    modification hearder  effet openning

--- scroll.js
+++ scroll.js
@@ -1,65 +1,68 @@
 //rezise on scroll
-
-window.addEventListener("resize", resizeScreen);
 window.onload = resizeScreen;
+window.addEventListener("resize", resizeScreen);
 
 //Resize
 function scrollFunctionOnPhone() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("topBar").style.height = "9.5rem";
-    document.getElementById("lvi").style.width = "15rem";
-    document.getElementById("lvi").style.left = "15%";
-    document.getElementById('logobar').style.margin ="0";
-      document.getElementById('headerBtn').style.margin ="0.5rem 0";
+    document.getElementById("downSolid").style.height = "0";
+    document.getElementById("lvi").style.marginTop = "4rem";
+    document.getElementById("lvi").style.width = "12rem";
+    //document.getElementById('logobar').style.margin ="0";
+    document.getElementById('headerBtn').style.margin ="0.5rem 0";
   } else {
-    document.getElementById("topBar").style.height = "12.4rem";
+    document.getElementById("topBar").style.height = "24rem";
+    document.getElementById("downSolid").style.height = "29rem";
+    document.getElementById("lvi").style.marginTop = "15rem";
     document.getElementById("lvi").style.width = "20rem";
-    document.getElementById("lvi").style.left = "5%";
-    document.getElementById('logobar').style.margin ="1rem 0";
-    document.getElementById('headerBtn').style.margin ="1.5rem 0 1.5rem 1.5rem";
+    //document.getElementById('logobar').style.margin ="1rem 0";
+    document.getElementById('headerBtn').style.margin ="25rem 10rem";
   }
 }
 
 function scrollFunctionOnTab() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("topBar").style.height = "9rem";
-      document.getElementById("lvi").style.width = "17rem";
-      document.getElementById("lvi").style.left = "30%";
-      document.getElementById('logobar').style.margin ="0.5rem 0";
-      document.getElementById('headerBtn').style.margin ="1rem 0";
+    document.getElementById("topBar").style.height = "8rem";
+    document.getElementById("downSolid").style.height = "0";
+    document.getElementById("lvi").style.marginTop = "0";
+    document.getElementById("lvi").style.width = "17rem";
+    //document.getElementById('logobar').style.margin ="0.5rem 0";
+    document.getElementById('headerBtn').style.margin ="1rem 0";
   } else {
-    document.getElementById("topBar").style.height = "12rem";
+    document.getElementById("topBar").style.height = "25rem";
+      document.getElementById("downSolid").style.height = "39rem";
+    document.getElementById("lvi").style.marginTop = "15rem";
     document.getElementById("lvi").style.width = "23rem";
-    document.getElementById("lvi").style.left = "25%";
-    document.getElementById('logobar').style.margin ="1rem 0";
-    document.getElementById('headerBtn').style.margin ="1.5rem 0 1.5rem 1.5rem";
+    //document.getElementById('logobar').style.margin ="1rem 0";
+    document.getElementById('headerBtn').style.margin ="23rem";
 
   }
 }
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("topBar").style.height = "8em";
-      document.getElementById("lvi").style.width = "17rem";
-      document.getElementById("lvi").style.left = "40%";
-      document.getElementById('logobar').style.margin ="0.5rem 0";
-      document.getElementById('headerBtn').style.margin ="1rem 0";
+    document.getElementById("topBar").style.height = "5.5em";
+    document.getElementById("downSolid").style.height = "0";
+    document.getElementById("lvi").style.width = "12rem";
+    document.getElementById('logobar').style.margin ="-4rem 3rem";
+    document.getElementById('headerBtn').style.margin ="1rem";
   } else {
-    document.getElementById("topBar").style.height = "12rem";
-    document.getElementById("lvi").style.width = "27rem";
-    document.getElementById("lvi").style.left = "35%";
+    document.getElementById("topBar").style.height = "17rem";
+    document.getElementById("downSolid").style.height = "29rem";
+    document.getElementById("lvi").style.width = "40rem";
     document.getElementById('logobar').style.margin ="3rem 0";
-    document.getElementById('headerBtn').style.margin ="3.5rem 0 3.5rem 3.5rem";
+    document.getElementById('headerBtn').style.margin =" 20rem 50rem";
   }
 }
 
 function resizeScreen(){
-  var screenSize = window.screen.width;
+  var screenSize = window.innerWidth;
   if (screenSize < 576 ){
       window.onscroll = function() {scrollFunctionOnPhone()};
   }else if (screenSize >= 576 && screenSize < 1199 ){
       window.onscroll = function() {scrollFunctionOnTab()};
   }else {
-    window.onscroll = function() {scrollFunction()};
+      window.onscroll = function() {scrollFunction()};
   }
 }
